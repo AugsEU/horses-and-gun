390fb1643cb3e02c38c983d0da8fac8f6cd4a317
El-Savoury <ross_morgan@outlook.com>
2022-07-16 20:34:05 +0100

    Added scrolling image class

---
+++ HorsesAndGun/ScrollingImage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace HorsesAndGun
+{
+    internal class ScrollingImage
+    {
+        private Texture2D mTexture1, mTexture2;
+        private Rectangle mTexture1Bounds, mTexture2Bounds;
+        private Vector2 mPosition;
+        private int mScrollSpeed;
+
+        public ScrollingImage(Texture2D texture1, Texture2D texture2, Vector2 position, int scrollSpeed)
+        {
+            mTexture1 = texture1;
+            mTexture2 = texture2;
+            mPosition = position;
+            mScrollSpeed = scrollSpeed;
+
+            mTexture1Bounds = new Rectangle((int)mPosition.X, (int)mPosition.Y, mTexture1.Width, mTexture1.Height);
+            mTexture2Bounds = new Rectangle((int)mPosition.X + mTexture1Bounds.Width, (int)mPosition.Y, mTexture2.Width, mTexture2.Height);
+        }
+
+        private void ScrollImages()
+        {
+            if (mTexture1Bounds.X + mTexture1Bounds.Width <= 0)
+            {
+                mTexture1Bounds.X = mTexture2Bounds.X + mTexture2Bounds.Width;
+            }
+            if (mTexture2Bounds.X + mTexture2Bounds.Width <= 0)
+            {
+                mTexture2Bounds.X = mTexture1Bounds.X + mTexture1Bounds.Width;
+            }
+        }
+
+        public void Update()
+        {
+            mTexture1Bounds.X -= mScrollSpeed;
+            mTexture2Bounds.X -= mScrollSpeed;
+            ScrollImages();
+        }
+
+        public void Draw(DrawInfo info)
+        {
+            info.spriteBatch.Draw(mTexture1, mTexture1Bounds, Color.White);
+            info.spriteBatch.Draw(mTexture2, mTexture2Bounds, Color.White);
+        }
+
+    }
+}
