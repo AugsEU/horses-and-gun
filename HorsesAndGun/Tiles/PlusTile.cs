baca1d097883fe1519d6c8626595f1a9bb62a0e7
adahlkvist <AugustDahlkvist@gmail.com>
2022-07-16 18:09:05 +0100

    Add PlusTile

---
+++ HorsesAndGun/Tiles/PlusTile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace HorsesAndGun.Tiles
+{
+    internal class PlusTile : BasicTile
+    {
+        public PlusTile(ContentManager content) : base(content, "Tiles/plus_one_tile")
+        {
+
+        }
+    }
+}
