2424e59e76ff37bf6ab88f9cdc1878dc6112f270
adahlkvist <AugustDahlkvist@gmail.com>
2022-07-17 15:52:26 +0100

    Add tile files

---
+++ HorsesAndGun/Tiles/DeathTile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace HorsesAndGun.Tiles
+{
+    internal class DeathTile : BasicTile
+    {
+        int mMoveAmount;
+
+        public DeathTile(ContentManager content, int _amount) : base(content, )
+        {
+            mMoveAmount = _amount;
+        }
+        public override void ApplyEffect(Horse horse, TrackManager trackManager)
+        {
+            SoundManager.I.PlaySFX(SoundManager.SFXType.TileActivate, 0.75f);
+            horse.Kill();
+        }
+    }
+}
